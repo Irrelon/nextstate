3e3af82f9e4a3259d5fe577d9af5bf45c67c4e01
Rob Evans <rob@irrelon.com>
2018-11-28 11:58:16 +0000

    Production build 1.0.0

---
+++ src/index.js
@@ -0,0 +1,121 @@
+/**
+ * Irrelon Lightstate - A very lightweight react-based state management library.
+ * MIT license.
+ */
+import Emitter from 'irrelon-emitter';
+import React from "react";
+
+/**
+ * The StateController class manages states including their data
+ * and mutations.
+ */
+class StateController {
+	constructor (data) {
+		this._data = data;
+	}
+	
+	update (data) {
+		if (!Object.is(this._data, data)) {
+			this._data = data;
+			this.emit('change');
+		}
+	}
+	
+	value () {
+		return this._data;
+	}
+	
+	find (query, options) {
+		return this._data;
+	}
+}
+
+// Give StateController's prototype the event emitter methods
+// and functionality
+Emitter(StateController);
+
+const mapToStateData = (obj, overrides = {}) => {
+	return Object.keys(obj).reduce((acc, key) => {
+		acc[key] = overrides[key] || obj[key].find();
+		
+		return acc;
+	}, {});
+};
+
+/**
+ * Wraps the given component in a HOC that controls the props for the wrapped
+ * component based on the stateControllerMap object passed in.
+ * @param {Object} stateControllerMap The object containing the key value pairs
+ * used to create props on the wrapped component. Each key becomes the prop name
+ * used to access the value (state controller) data.
+ * @param {React.Component} ComponentToWrap The react component to wrap in this
+ * HOC so that it will receive new props when state data changes.
+ * @returns {Class<React.Component>} The new HOC.
+ */
+const useProps = (stateControllerMap, ComponentToWrap) => {
+	return class LightStateHOC extends React.Component {
+		static getInitialProps (ctx) {
+			console.log("Wrapped component getInitialProps");
+			
+			if (ComponentToWrap.getInitialProps) {
+				return ComponentToWrap.getInitialProps(ctx)
+					.then((dataProps) => {
+						const stateData = mapToStateData(stateControllerMap);
+						
+						return {
+							...dataProps,
+							...stateData
+						};
+					});
+			} else {
+				return mapToStateData(stateControllerMap);
+			}
+		}
+		
+		constructor (props) {
+			super(props);
+			console.log('Wrapped component constructor', props);
+			const stateData = mapToStateData(stateControllerMap, props);
+			this._changeHandlers = {};
+			
+			this.state = {
+				...stateData
+			};
+		}
+		
+		componentDidMount () {
+			Object.keys(stateControllerMap).forEach((key) => {
+				this._changeHandlers[key] = this.generateHandleChangeByKey(this, key, stateControllerMap[key]);
+				stateControllerMap[key].on('change', this._changeHandlers[key]);
+			});
+		}
+		
+		componentWillUnmount () {
+			Object.keys(stateControllerMap).forEach((key) => {
+				stateControllerMap[key].off('change', this._changeHandlers[key]);
+			});
+		}
+		
+		generateHandleChangeByKey (componentInstance, key, stateController) {
+			return function () {
+				componentInstance.setState({
+					[key]: stateController.find()
+				});
+			};
+		}
+		
+		render () {
+			return (
+				<ComponentToWrap
+			{...this.props}
+			{...this.state}
+			/>
+		);
+		}
+	};
+};
+
+export {
+	StateController,
+	useProps
+};
