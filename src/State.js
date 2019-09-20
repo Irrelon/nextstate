cbd45e3ee8b37646232c60b112cc9491d3fc2f97
Rob Evans <rob@irrelon.com>
2019-09-20 02:55:52 +0100

    Fixed call

--- src/State.js
+++ src/State.js
@@ -35,7 +35,7 @@ class State {
 			throw new Error("Cannot set() without passing a path argument!");
 		}
 		
-		return store.value(store.set(pathJoin(this._name, path), newVal, options));
+		return this.value(store.set(pathJoin(this._name, path), newVal, options));
 	}
 	
 	update (store, newVal, options) {
